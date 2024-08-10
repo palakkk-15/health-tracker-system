354b9f8872be8dbccf43135e6be0df5b2942234f
palakkk-15 <palakmodi00@gmail.com>
2024-08-10 15:29:16 +0530

    Added Files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import mycomponents from './components/mycomponents.js';
 function App() {
     return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
